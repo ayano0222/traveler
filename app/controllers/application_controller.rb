081df2c2d30c69816dd953caff1c8850c5b97c1e
ayano0222 <199382040+ayano0222@users.noreply.github.com>
2025-04-17 23:15:33 +0900

    ちょこっと

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
 
-    before_action :configre_permitted_permitted_parameters, if: :devise_controller?
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
     protected
-    def configre_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     end
         
 end
