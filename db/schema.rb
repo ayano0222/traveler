307cc496081636a6edb7eb7822f98365adfb3992
ayano0222 <199382040+ayano0222@users.noreply.github.com>
2025-05-13 22:58:42 +0900

    routes前まで

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_11_135100) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_13_133856) do
   create_table "comments", force: :cascade do |t|
     t.text "comment"
     t.integer "user_id"
@@ -43,6 +43,11 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_11_135100) do
     t.datetime "updated_at", null: false
     t.index ["followed_id"], name: "index_relationships_on_followed_id"
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
